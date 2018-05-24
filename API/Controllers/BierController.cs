e7c600f03c2937b2c008caea5916abb55ffe501a
Robbe Vermeire <robbe.vermeire@student.ap.be>
2018-05-24 15:03:16 +0200

    Enkele verbs toegevoegd aan de Bier controller

--- API/Controllers/BierController.cs
+++ API/Controllers/BierController.cs
@@ -4,12 +4,12 @@ using Microsoft.AspNetCore.Mvc;
 using Model;
 
 [Route("api/v1/bier")]
-public class BierController: Controller
+public class BierController : Controller
 {
     private readonly BierContext context;
     public BierController(BierContext context)
     {
-        this.context=context;
+        this.context = context;
     }
 
     [HttpGet]
@@ -17,4 +17,53 @@ public class BierController: Controller
     {
         return context.Bieren.ToList();
     }
+
+    [HttpPost]
+    public IActionResult CreateBook([FromBody] Bier newBier)
+    {
+        context.Bieren.Add(newBier);
+        context.SaveChanges();
+        return Created("", newBier);
+    }
+
+    [HttpPut]
+    public IActionResult UpdateBier([FromBody] Bier updateBier)
+    {
+        var orgBier = context.Bieren.Find(updateBier.Id);
+        if(bier == null)
+            return NotFound();
+        orgBier.Naam = updateBier.Naam;
+        orgBier.EBC = updateBier.EBC;
+        orgBier.EBU = updateBier.EBU;
+        orgBier.Kleur = updateBier.Kleur;
+        orgBier.Brouwerij = updateBier.Brouwerij;
+        orgBier.AlcoholGehalte = updateBier.AlcoholGehalte;
+
+        context.SaveChanges();
+        return Ok(orgBier);
+
+    }
+
+    [Route("{id}")]
+    [HttpGet]
+    public IActionResult GetBier(int id)
+    {
+        var bier = context.Bieren.Find(id);
+        if (bier == null)
+            return NotFound();
+        return Ok(bier);
+    }
+    [HttpDelete]
+    public IActionResult DeleteBier(int id)
+    {
+        var bier = context.Bieren.Find(id);
+        if (bier == null)
+            return NotFound();
+        context.Bieren.Remove(bier);
+        context.SaveChanges();
+        return NoContent();
+
+    }
+
+
 }
